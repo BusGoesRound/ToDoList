302a4316131a5f33cf892643dcf0f9e5dc05852b
Bus <bookwormtwenty@gmail.com>
2025-08-26 23:24:34 -0400

    Added Grab Dots (Unimplemented)

--- UserControls/Task.xaml.cs
+++ UserControls/Task.xaml.cs
@@ -9,120 +9,125 @@ namespace ToDoList.UserControls
 {
 	public partial class Task : UserControl
 	{
-        private readonly Brush BackgroundColor = (SolidColorBrush)Application.Current.Resources["ForegroundColor"];
+		private readonly Brush BackgroundColor = (SolidColorBrush)Application.Current.Resources["ForegroundColor"];
 
-        public bool IsChecked = false;
-        public bool RepeatDaily = false;
+		public bool IsChecked = false;
+		public bool RepeatDaily = false;
 
-        // Constructors ______________________________________________________________________________________________________________
+		// Constructors ______________________________________________________________________________________________________________
 
-        public Task()
+		public Task()
 		{
-            InitializeComponent();
-            DataContext = this;
+			InitializeComponent();
+			DataContext = this;
 
-            TaskText = "Task";
-        }
+			TaskText = "Task";
+		}
 
-        public Task(string task)
-        {
-            InitializeComponent();
-            DataContext = this;
+		public Task(string task)
+		{
+			InitializeComponent();
+			DataContext = this;
 
-            string[] taskString = task.Split('|');
+			string[] taskString = task.Split('|');
 
-            IsChecked = bool.Parse(taskString[1]);
-            if(taskString.Length == 3){ RepeatDaily = bool.Parse(taskString[2]); UpdateRepeatDaily(); }
-            UpdateCheckBox();
-            TaskText = taskString[0];
-        }
+			IsChecked = bool.Parse(taskString[1]);
+			if(taskString.Length == 3){ RepeatDaily = bool.Parse(taskString[2]); UpdateRepeatDaily(); }
+			UpdateCheckBox();
+			TaskText = taskString[0];
+		}
 
-        public Task(string taskText, bool isChecked)
-        {
-            InitializeComponent();
-            DataContext = this;
+		public Task(string taskText, bool isChecked)
+		{
+			InitializeComponent();
+			DataContext = this;
 
-            IsChecked = isChecked;
-            UpdateCheckBox();
-            TaskText = taskText;
-        }
+			IsChecked = isChecked;
+			UpdateCheckBox();
+			TaskText = taskText;
+		}
 
-        // ToString ______________________________________________________________________________________________________________
+		// ToString ______________________________________________________________________________________________________________
 
-        override public string ToString()
-        {
-            return TaskText + "|" + IsChecked + "|" + RepeatDaily;
-        }
+		override public string ToString()
+		{
+			return TaskText + "|" + IsChecked + "|" + RepeatDaily;
+		}
 
-        //Checkbox ______________________________________________________________________________________________________________
+		//Checkbox ______________________________________________________________________________________________________________
 
-        private void CheckBoxClicked(object sender, RoutedEventArgs e)
+		private void CheckBoxClicked(object sender, RoutedEventArgs e)
 		{
 			IsChecked = !IsChecked;
-            UpdateCheckBox();
-        }
+			UpdateCheckBox();
+		}
 
-        public void UpdateCheckBox()
-        {
-            if (IsChecked) DotColor = BackgroundColor;
-            else DotColor = Brushes.Transparent;
-        }
+		public void UpdateCheckBox()
+		{
+			if (IsChecked) DotColor = BackgroundColor;
+			else DotColor = Brushes.Transparent;
+		}
 
-        //Repeat ______________________________________________________________________________________________________________
+		//Repeat ______________________________________________________________________________________________________________
 
-        private void RepeatCheckbox(object sender, RoutedEventArgs e)
-        {
-            RepeatDaily = !RepeatDaily;
-            UpdateRepeatDaily();
-        }
+		private void RepeatCheckbox(object sender, RoutedEventArgs e)
+		{
+			RepeatDaily = !RepeatDaily;
+			UpdateRepeatDaily();
+		}
 
-        private void UpdateRepeatDaily()
-        {
-            if (RepeatDaily) DotColor2 = BackgroundColor;
-            else DotColor2 = Brushes.Transparent;
-        }
+		private void UpdateRepeatDaily()
+		{
+			if (RepeatDaily) DotColor2 = BackgroundColor;
+			else DotColor2 = Brushes.Transparent;
+		}
 
-        public void ResetTask()
-        {
-            if (RepeatDaily)
-            {
-                IsChecked = false;
-                UpdateCheckBox();
-            }
-        }
+		public void ResetTask()
+		{
+			if (RepeatDaily)
+			{
+				IsChecked = false;
+				UpdateCheckBox();
+			}
+		}
 
-        //Delete ______________________________________________________________________________________________________________
+		//Delete ______________________________________________________________________________________________________________
 
-        private void DeleteClicked(object sender, RoutedEventArgs e)
-        {
-            if (Parent is StackPanel parentStackPanel)
-            {
-                parentStackPanel.Children.Remove(this);
-            }
-        }
+		private void DeleteClicked(object sender, RoutedEventArgs e)
+		{
+			if (Parent is StackPanel parentStackPanel)
+			{
+				parentStackPanel.Children.Remove(this);
+			}
+		}
 
-        // Dependency Properties ______________________________________________________________________________________________________________
+		// Dependency Properties ______________________________________________________________________________________________________________
 
-        private static readonly DependencyProperty DotColorProperty = DependencyProperty.Register("DotColor", typeof(Brush), typeof(Task));
-        private static readonly DependencyProperty DotColor2Property = DependencyProperty.Register("DotColor2", typeof(Brush), typeof(Task));
-        private static readonly DependencyProperty TaskTextProperty = DependencyProperty.Register("TaskText", typeof(string), typeof(Task));
+		private static readonly DependencyProperty DotColorProperty = DependencyProperty.Register("DotColor", typeof(Brush), typeof(Task));
+		private static readonly DependencyProperty DotColor2Property = DependencyProperty.Register("DotColor2", typeof(Brush), typeof(Task));
+		private static readonly DependencyProperty TaskTextProperty = DependencyProperty.Register("TaskText", typeof(string), typeof(Task));
 
-        public Brush DotColor
+		public Brush DotColor
 		{
 			get { return (Brush)GetValue(DotColorProperty); }
 			set { SetValue(DotColorProperty, value); }
-        }
+		}
 
-        public Brush DotColor2
-        {
-            get { return (Brush)GetValue(DotColor2Property); }
-            set { SetValue(DotColor2Property, value); }
-        }
+		public Brush DotColor2
+		{
+			get { return (Brush)GetValue(DotColor2Property); }
+			set { SetValue(DotColor2Property, value); }
+		}
 
-        public string TaskText
-        {
-            get { return (string)GetValue(TaskTextProperty); }
-            set { SetValue(TaskTextProperty, value); }
-        }
-    }
+		public string TaskText
+		{
+			get { return (string)GetValue(TaskTextProperty); }
+			set { SetValue(TaskTextProperty, value); }
+		}
+
+		private void MoveTask(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
+		{
+			
+		}
+	}
 }
